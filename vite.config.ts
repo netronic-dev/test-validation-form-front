ccb8d32a3f5f07f597e371c723444ef6232fefc2
Albina Anistratenko <albuska2013@gmail.com>
2024-05-24 09:51:42 +0300

    test two

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-validation-form-front",
+  base: "/test-validation-form-front/",
   server: {
-    open: "/test-validation-form-front",
+    open: "/test-validation-form-front/",
     port: 3000,
   },
 });
