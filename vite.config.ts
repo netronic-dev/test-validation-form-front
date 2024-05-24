bd10427f2dd207e720aa017b0b38650eb305bd18
Albina Anistratenko <albuska2013@gmail.com>
2024-05-24 09:14:48 +0300

    delete not need files, setup gh-pages deploy

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/test-validation-form-front",
+  server: {
+    open: "/test-validation-form-front",
+    port: 3000,
+  },
+});
