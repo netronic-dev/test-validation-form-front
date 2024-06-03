78121d2a86d8ed05f435028fbdaf87a5f649af79
Albina Anistratenko <albuska2013@gmail.com>
2024-06-03 15:53:29 +0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
 import { IFormData } from "../models";
 
 export const addLead = async (formData: IFormData) => {
-    console.log(formData, "formData");
+  console.log(formData, "formData");
   const response = await fetch(
-    "https://test-validation-form-backend.vercel.app/form",
+    // "https://test-validation-form-backend.vercel.app/form",
+    "http://localhost:5000/form",
     {
       method: "POST",
       headers: {
