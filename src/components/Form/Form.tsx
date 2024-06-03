7d3cb644d1a6c14520df7b3e85f418559e5d1349
Albina Anistratenko <albuska2013@gmail.com>
2024-06-03 16:25:34 +0300

    fix protocol

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -104,11 +104,11 @@ const Form = () => {
   useEffect(() => {
     const fetchIpAndGeoInfo = async () => {
       try {
-        const ipResponse = await fetch("http://api.ipify.org?format=json");
+        const ipResponse = await fetch("https://api.ipify.org?format=json");
         const ipData = await ipResponse.json();
         setIpAddress(ipData.ip);
 
-        const geoResponse = await fetch(`http://ip-api.com/json/${ipData.ip}`);
+        const geoResponse = await fetch(`https://ip-api.com/json/${ipData.ip}`);
         const geoData = await geoResponse.json();
         setGeoInfo(geoData);
       } catch (error) {
